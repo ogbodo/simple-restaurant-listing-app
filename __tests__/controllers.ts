00e8823d451d233af89925209433063a2657eb34
Izuking <matthiasogbodo@gmail.com>
2019-11-16 09:58:20 +0100

    Add test to assert that restaurants are returned based on opening state

--- __tests__/controllers.ts
+++ __tests__/controllers.ts
@@ -1,8 +1,15 @@
-import { getRestaurants } from '../src/controllers/index';
+import {
+  getRestaurants,
+  sortRestaurantsByOpeningState,
+} from '../src/controllers/index';
 
 describe('Returns All Restaurants', () => {
   test('Should get all restaurants', () => {
     const restaurants = getRestaurants();
+    expect(restaurants).toBeDefined();
+  });
+  test('Should return all restaurants sorted by opening state', () => {
+    const restaurants = sortRestaurantsByOpeningState();
     expect(restaurants).toBeDefined();
   });
 });

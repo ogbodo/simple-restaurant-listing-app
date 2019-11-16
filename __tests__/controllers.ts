0a86557adc70a05cff27e8b296b0125a37f49ce4
Izuking <matthiasogbodo@gmail.com>
2019-11-16 16:00:56 +0100

    Added test to ensure restaurants can be sorted by bestMatch

--- __tests__/controllers.ts
+++ __tests__/controllers.ts
@@ -1,6 +1,7 @@
 import {
   getRestaurants,
   sortRestaurantsByOpeningState,
+  sortRestaurantsByValues,
 } from '../src/controllers/index';
 
 describe('Returns All Restaurants', () => {
@@ -11,5 +12,13 @@ describe('Returns All Restaurants', () => {
   test('Should return all restaurants sorted by opening state', () => {
     const restaurants = sortRestaurantsByOpeningState();
     expect(restaurants).toBeDefined();
+  });
+  describe('Returns all restaurants sorted by values', () => {
+    test('Should return all restaurants sorted by best Match', () => {
+      const restaurants = sortRestaurantsByValues('bestMatch');
+      console.log(restaurants);
+
+      expect(restaurants).toBeDefined();
+    });
   });
 });

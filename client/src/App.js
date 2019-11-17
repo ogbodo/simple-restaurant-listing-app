54e273a9b654a0dac37012ee8531a2689465ae6b
Izuking <matthiasogbodo@gmail.com>
2019-11-17 14:59:05 +0100

    imported component that get populated with list of restaurants

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'; //Import styles for semantic UI
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import './App.css'; //Imports my own css file
+
+//components
+import RestaurantList from './components/RestaurantList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RestaurantList />
     </div>
   );
 }

74b09ca1cf4ac1226bd3b0137e51a30fdec30e07
Izuking <matthiasogbodo@gmail.com>
2019-11-17 13:33:54 +0100

    declared all constant values in a file

---
+++ client/src/helpers.js/constants.js
@@ -0,0 +1,54 @@
+export const sortValues = [
+  {
+    key: '',
+    text: 'None',
+    value: '',
+  },
+  {
+    key: 'bestMatch',
+    text: 'Best Match',
+    value: 'bestMatch',
+  },
+  {
+    key: 'newest',
+    text: 'Newest',
+    value: 'newest',
+  },
+  {
+    key: 'ratingAverage',
+    text: 'Rating Average',
+    value: 'ratingAverage',
+  },
+  {
+    key: 'distance',
+    text: 'Distance',
+    value: 'distance',
+  },
+  {
+    key: 'popularity',
+    text: 'Popularity',
+    value: 'popularity',
+  },
+  {
+    key: 'averageProductPrice',
+    text: 'Average Product Price',
+    value: 'averageProductPrice',
+  },
+  {
+    key: 'deliveryCosts',
+    text: 'Delivery Costs',
+    value: 'deliveryCosts',
+  },
+  {
+    key: 'minCost',
+    text: 'Minimum Costs',
+    value: 'minCost',
+  },
+  {
+    key: 'topRestaurants',
+    text: 'Top Restaurants',
+    value: 'topRestaurants',
+  },
+];
+
+export const FAVORITES = 'favorites';
